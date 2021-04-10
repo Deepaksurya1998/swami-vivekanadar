66b326939aff0e450b0a50a3f8e0beb0cd0ff5ae
Deepak Surya .R <34158240+Deepaksurya1998@users.noreply.github.com>
2021-04-10 11:13:58 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,6 @@ export const environment = {
   production: true,
 
   firebaseConfig:{
-    apiKey: "AIzaSyCaVokM0c6G5TM9TaF0F1o38DgEeEdvMq8",
-    authDomain: "swami-vivekanandar.firebaseapp.com",
-    projectId: "swami-vivekanandar",
-    storageBucket: "swami-vivekanandar.appspot.com",
-    messagingSenderId: "611062833356",
-    appId: "1:611062833356:web:b149aaf8268b48b48a2269",
-    measurementId: "G-JR70P2S0M1"
+
   },
 };
